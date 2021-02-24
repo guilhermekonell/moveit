6d1e18226b7909be1c420b19e7c62e87e32c896e
Guilherme Konell <gui.ri.konell@hotmail.com>
2021-02-24 15:38:39 -0300

    Dia 1, 2 e 3 NLW 4

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/global.css'
+
+import { ChallengeProvider, ChallengesContext } from '../contexts/ChallengesContext';
+import { useState } from 'react';
+
+function MyApp({ Component, pageProps }) {
+
+
+  return (
+    <ChallengeProvider>
+      <Component {...pageProps} />
+    </ChallengeProvider>
+  )
+}
+
+export default MyApp
